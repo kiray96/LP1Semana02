9e678abdb8b831fbb5bfb4077158bd34b82082d6
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-02-26 22:09:34 +0000

    Create project SmoothOperator that shows increment and decrement operations

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Ask the player to insert an integral number
+            Console.Write("Insere número inteiro: ");
+
+            // Read the input of the player
+            string number = Console.ReadLine();
+
+            // Convert the string in to a sbyte
+            sbyte n = sbyte.Parse(number);
+
+            // Print the number with the decrement and increment
+            Console.WriteLine(--n);
+            Console.WriteLine(++n);
+
+        }
+    }
+}
