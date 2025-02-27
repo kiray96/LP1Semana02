61159cb775fd35490713f46bdf426818e49cd1ae
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-02-27 22:52:40 +0000

    Add questions to determine the appropriate responses

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -15,6 +15,30 @@ namespace ChatGPTOne
                 Console.Write("Place your question: ");
                 question = Console.ReadLine(); 
 
+                 //Check if it is a known issue and determine an appropriate
+                 // response
+                switch (question)
+                {
+                    case "What's your name?":
+                        response = "Megan";
+                        break;
+                    case "How old are you?":
+                        response = "3 years old";
+                        break;
+                    case "Are you a ghost?":
+                        response = "Yes and you?";
+                        break;
+                    case "You want to kill me?":
+                        response = "Obviously!";
+                        break;
+                    case "Are you alive?":
+                        response = "I don't know, am I?";
+                        break;
+                    default:
+                        response = "I don't know that information sorry";
+                        break;
+                }
+
             }
         }
     }
