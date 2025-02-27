d96efce643cb1b8e1fea535f080694b14d0cb690
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-02-27 22:49:11 +0000

    Add auxiliary variables and made the main loop

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ChatGPTOne
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Auxiliary variables
+            string question, response;
+
+            while (true)
+            {
+                // Ask the user a question
+                Console.Write("Place your question: ");
+                question = Console.ReadLine(); 
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+            
