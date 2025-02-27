f77b521c177dd829c932f5772b7cbdf3ec8c922e
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-02-27 19:37:36 +0000

    Format the the doubles into 3 decimal places and print the results

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -8,15 +8,14 @@ namespace Cilindro
         {
             // Ask for the user for the height input 
             Console.Write("Insere número com a altura do cilindro: ");
-
             // Read the input
             string altura = Console.ReadLine();
 
             // Ask for the user for the radius input 
             Console.Write("Insere número com o raio do cilindro: ");
-
             // Read the input
             string raio = Console.ReadLine();
+
 
 
 
@@ -27,12 +26,25 @@ namespace Cilindro
             double r = double.Parse(raio);
 
 
+
+
             // Calculate the volume of the cylinder
             // Using Math.PI for the PI
             // Using Math.Pow to raise by 2 
             double v = Math.PI * Math.Pow(r, 2) * a;
             // Calculate the surface area of the cylinder
             double s = 2 * Math.PI * r * (r + a);
+
+
+
+
+            // Format the the doubles into 3 decimal places 
+            string volume = $"{v:f3}";
+            string superficie = $"{s:f3}";
+
+            Console.Write(volume);
+            Console.Write(superficie);
+            
 
         }
     }
