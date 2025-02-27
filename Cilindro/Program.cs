9e8fdaef68806ac777937640fc95968b45749d52
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-02-27 19:29:47 +0000

    Calculate volume and surface area of the cylinder

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -18,11 +18,21 @@ namespace Cilindro
             // Read the input
             string raio = Console.ReadLine();
 
+
+
             // Convert the height input in to a double
             double a = double.Parse(altura);
 
             // Convert the radius input in to a double
             double r = double.Parse(raio);
+
+
+            // Calculate the volume of the cylinder
+            // Using Math.PI for the PI
+            // Using Math.Pow to raise by 2 
+            double v = Math.PI * Math.Pow(r, 2) * a;
+            // Calculate the surface area of the cylinder
+            double s = 2 * Math.PI * r * (r + a);
 
         }
     }
