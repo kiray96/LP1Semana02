ed75a28165dd025a22c36b3ea5cffcd842a6f5e2
kiray96 <inesmartinsbarradas1996@gmail.com>
2025-02-27 19:58:07 +0000

    Create variables with the Min and Max value of all types

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Create variables with the Min and Max value of all types 
+            sbyte sbmax = sbyte.MaxValue;
+            sbyte sbmin = sbyte.MinValue;
+
+            double dbmax = double.MaxValue;
+            double dbmin = double.MinValue;
+
+            ulong ulmax = ulong.MaxValue;
+            ulong ulmin = ulong.MinValue;
+
+            long lmax = long.MaxValue;
+            long lmin = long.MinValue;
+
+            float fmax = float.MaxValue;
+            float fmin = float.MinValue;
+
+            int imax = int.MaxValue;
+            int imin = int.MinValue;
+
+            short smax = short.MaxValue;
+            short smin = short.MinValue;
+
+            decimal decmax = decimal.MaxValue;
+            decimal decmin = decimal.MinValue;
+
+            byte bmax = byte.MaxValue;
+            byte bmin = byte.MinValue;
+
+            ushort usmax = ushort.MaxValue;
+            ushort usmin = ushort.MinValue;
+
+            uint uimax = uint.MaxValue;
+            uint uimin = uint.MaxValue;
+
+            char chmax = char.MaxValue;
+            char chmin = char.MinValue;
+        }
+    }
+}
